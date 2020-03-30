8ac73278f008f347618fc461e5e87509f8264feb
Stu Campbell <stucampbell.git@gmail.com>
2020-03-29 23:55:00 -0700

    updated to json from yaml and more prep for modularity

--- src/message.py
+++ src/message.py
@@ -1,15 +1,13 @@
 # Utility module for use with PythOnBoarding's Router layer
 """
-Python Slack Message class for use with the pythOnBoarding bot
+Python Slack Message class for use with the bot
 """
-# TODO: Fix hack with yaml library for handling unicode encoding issues
-import yaml
+import json
 
 
 class Message(object):
     """
-    Instantiates a Message object to create and manage
-    Slack onboarding messages.
+    Instantiates a Message object.
     """
     def __init__(self):
         super(Message, self).__init__()
@@ -27,11 +25,11 @@ class Message(object):
     def create_attachments(self):
         """
         Open JSON message attachments file and create attachments for
-        onboarding message. Saves a dictionary of formatted attachments on
+        message. Saves a dictionary of formatted attachments on
         the bot object.
         """
         with open('welcome.json') as json_file:
-            json_dict = yaml.safe_load(json_file)
+            json_dict = json.loads(json_file)
             json_attachments = json_dict["attachments"]
             [self.attachments[i].update(json_attachments[i]) for i
              in range(len(json_attachments))]
