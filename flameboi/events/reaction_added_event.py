e4bb481c60b21cf4bbcda47fae38f357c9030fa0
Stu Campbell <stucampbell.git@gmail.com>
2020-07-11 11:00:11 -0700

    fixed non-function calls in event handlers

--- flameboi/events/reaction_added_event.py
+++ flameboi/events/reaction_added_event.py
@@ -8,9 +8,9 @@ class ReactionAddedEvent(Event):
 
     def get_details(self) -> dict:
         return {
-            'user_id': self.get_user_id,
-            'channel_id': self.get_item_channel,
-            'text': self.get_text,
-            'ts': self.get_item_ts,
-            'reaction': self.get_reaction
+            'user_id': self.get_user_id(),
+            'channel_id': self.get_item_channel(),
+            'text': self.get_text(),
+            'ts': self.get_item_ts(),
+            'reaction': self.get_reaction(),
         }
